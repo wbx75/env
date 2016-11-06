d09ee699badac0b8084d5369f6534b52e6fd6ca4
xxx <xxx>
2016-11-06 15:25:33 +0100

    Add .bash_logout file

--- configure.sh
+++ configure.sh
@@ -4,5 +4,6 @@ ENV_LOCATION=$(pwd)
 
 ln -s $ENV_LOCATION/.bashrc $HOME/.bashrc
 ln -s $ENV_LOCATION/.bash_profile $HOME/.bash_profile
+ln -s $ENV_LOCATION/.bash_logout $HOME/.bash_logout
 ln -s $ENV_LOCATION/.vim $HOME/.vim
 
